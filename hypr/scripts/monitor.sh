3a9d1d99e74c0780facbb9a41860c3b982af363b
bcsaalinas <betocnos2006@gmail.com>
2026-08-11 23:54:27 -0600

    fix monitor overlap, DP-1 at 200Hz, park eDP-1 clear of externals

--- hypr/scripts/monitor.sh
+++ hypr/scripts/monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if hyprctl monitors | grep -q "HDMI-A-1"; then
+if hyprctl monitors | grep -qE "Monitor (HDMI-A-1|DP-1)"; then
     hyprctl keyword monitor "eDP-1, disable"
 else
-    hyprctl keyword monitor "eDP-1, 1920x1080@144, 0x0, 1"
+    hyprctl keyword monitor "eDP-1, 1920x1080@144, 5120x0, 1"
 fi
